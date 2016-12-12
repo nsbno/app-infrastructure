06a9904f4bee23091134591d3015352d874f0be3
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2016-12-12 09:16:41 +0100

    map_public_ip_on_launch default false

--- networking/subnet/vars.tf
+++ networking/subnet/vars.tf
@@ -2,5 +2,5 @@ variable "vpc_id" {}
 variable "number_of_subnets" {}
 variable "zones" { type = "map" }
 variable "cidr_blocks" { type = "map" }
-variable "map_public_ip_on_launch" {}
+variable "map_public_ip_on_launch" { default = "false" }
 variable "name" {}
