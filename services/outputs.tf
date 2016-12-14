017a2fba3eaca03a4f94c8d20dbc86f1c15ad7cc
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2016-12-14 08:23:07 +0100

    renamed security_group

--- services/outputs.tf
+++ services/outputs.tf
@@ -2,6 +2,6 @@ output "app_ids" {
   value = ["${aws_instance.app.*.id}"]
 }
 
-output "appserver_security_group_id" {
+output "security_group_id" {
   value = "${aws_security_group.appserver.id}"
 }
