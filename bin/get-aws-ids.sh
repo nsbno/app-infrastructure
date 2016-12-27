b352b1ffde00811498b6e19a16f5b871d5126105
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2016-12-27 17:00:58 +0100

    renamed cronserver to mgmtserver

--- bin/get-aws-ids.sh
+++ bin/get-aws-ids.sh
@@ -4,7 +4,7 @@ vpcid=$(aws ec2 describe-vpcs --filters "Name=tag-value, Values=${1}_vpc" | jq '
 private_subnets=$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=${vpcid}, Name=tag-value, Values=*private_subnet*" | jq '.Subnets[].SubnetId' | tr '\n' ',' | sed s/,$//)
 public_subnets=$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=${vpcid}, Name=tag-value, Values=*public_subnet*" | jq '.Subnets[].SubnetId' | tr '\n' ',' | sed s/,$//)
 bastion_security_group_id=$(aws ec2 describe-security-groups --filters "Name=vpc-id, Values=${vpcid}, Name=tag-value, Values=*bastion_ssh_sg*" | jq '.SecurityGroups[0].GroupId')
-cronserver_security_group_id=$(aws ec2 describe-security-groups --filters "Name=vpc-id, Values=${vpcid}, Name=tag-value, Values=*cronserver_sg*" | jq '.SecurityGroups[0].GroupId')
+mgmtserver_security_group_id=$(aws ec2 describe-security-groups --filters "Name=vpc-id, Values=${vpcid}, Name=tag-value, Values=*mgmtserver_sg*" | jq '.SecurityGroups[0].GroupId')
 nat_ip=$(aws ec2 describe-nat-gateways --filter "Name=vpc-id, Values=${vpcid}" | jq ".NatGateways[0].NatGatewayAddresses[0].PublicIp")
 
 echo "
@@ -16,6 +16,6 @@ variable \"public_subnet_ids\" {
   default = [$public_subnets]
 }
 variable \"bastion_security_group_id\" { default = $bastion_security_group_id }
-variable \"cronserver_security_group_id\" { default = $cronserver_security_group_id }
+variable \"mgmtserver_security_group_id\" { default = $mgmtserver_security_group_id }
 variable \"nat_ip\" { default = ${nat_ip} }
 "
