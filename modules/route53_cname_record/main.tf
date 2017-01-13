f66bce478a0ab92392f9141671e4466e221e9395
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2017-01-13 11:30:57 +0100

    parameterized route53 record

--- modules/route53_cname_record/main.tf
+++ modules/route53_cname_record/main.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "environment_route53_record" {
-  zone_id        = "${var.route53_hosted_zone_id}"
-  name           = "${var.route53_record_name}"
-  type           = "CNAME"
-  ttl            = "${var.route53_record_ttl}"
-  records        = ["${var.appname}-${var.env}.${var.aws_region}.elasticbeanstalk.com"]
+  zone_id = "${var.route53_hosted_zone_id}"
+  name    = "${var.route53_record_name}"
+  type    = "CNAME"
+  ttl     = "${var.route53_record_ttl}"
+  records = ["${var.route53_record}"]
 }
--- modules/route53_cname_record/vars.tf
+++ modules/route53_cname_record/vars.tf
@@ -2,5 +2,6 @@ variable "env" {}
 variable "appname" {}
 variable "aws_region" {}
 variable "route53_record_name" {}
+variable "route53_record" {}
 variable "route53_record_ttl" {}
 variable "route53_hosted_zone_id" {}
