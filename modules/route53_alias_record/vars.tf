fca60f8129ced49b1d6f393f35689ef27e62329b
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2017-01-25 13:30:24 +0100

    removed route53_alias_record module. It is not used anymore

--- modules/route53_alias_record/vars.tf
+++
@@ -1,7 +0,0 @@
-variable "env" {}
-variable "appname" {}
-variable "aws_region" {}
-variable "route53_record_name" {}
-variable "route53_hosted_zone_id" {}
-variable "elb_hosted_zone_id" {}
-variable "alias_name" {}
