f37a68af727fcde5d69fe141663365966acdda8c
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2017-01-26 10:39:47 +0100

    db backup and multi az

--- modules/database/vars.tf
+++ modules/database/vars.tf
@@ -1,24 +1,16 @@
 variable "db_name" {}
-
 variable "db_subnet_group_id" {}
-
 variable "db_identifier" {}
-
 variable "db_storage" {}
-
 variable "db_engine" {}
-
 variable "db_engine_version" {}
-
 variable "db_instance_class" {}
-
 variable "db_username" {}
-
 variable "db_password" {}
-
 variable "db_parameter_group_name" {}
-
 variable "vpc_id" {}
-
 variable "db_sg_name" {}
-
+variable "backup_retention_period" {}
+variable "multi_az" {}
+variable "backup_window" {}
+variable "allocated_storage" {}
