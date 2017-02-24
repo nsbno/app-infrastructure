01755006879c07481c9db68b1da0880a7d9c52d7
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2017-02-24 09:36:27 +0100

    use db address instead of db endpoint for dns

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -2,6 +2,6 @@ output "db_security_group_id" {
     value = "${aws_security_group.db_sg.id}"
 }
 
-output "endpoint" {
-    value = "${aws_db_instance.db.endpoint}"
+output "address" {
+    value = "${aws_db_instance.db.address}"
 }
