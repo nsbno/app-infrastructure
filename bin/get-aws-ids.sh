12456d8209758f868554bbe19710f63d4a104c87
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2017-05-03 16:50:21 +0200

    added app security id to bin/get-aws-ids.sh

--- bin/get-aws-ids.sh
+++ bin/get-aws-ids.sh
@@ -7,6 +7,7 @@ bastion_security_group_id=$(aws ec2 describe-security-groups --filters "Name=vpc
 mgmtserver_security_group_id=$(aws ec2 describe-security-groups --filters "Name=vpc-id, Values=${vpcid}, Name=tag-value, Values=${1}_mgmtserver_sg*" | jq '.SecurityGroups[0].GroupId')
 nat_ip=$(aws ec2 describe-nat-gateways --filter "Name=vpc-id, Values=${vpcid}" | jq ".NatGateways[0].NatGatewayAddresses[0].PublicIp")
 route53_hosted_zone_id=$(aws route53 list-hosted-zones-by-name --dns-name ${2} | jq '.HostedZones[0].Id' | sed 's/\/hostedzone\///g')
+app_security_group_id=$(aws ec2 describe-security-groups --filters "Name=vpc-id, Values=${vpcid}, Name=tag-value, Values=nsbno-${1}" | jq '.SecurityGroups[0].GroupId')
 
 echo "
 variable \"vpc_id\" { default=\"$vpcid\" }
@@ -20,4 +21,5 @@ variable \"bastion_security_group_id\" { default = $bastion_security_group_id }
 variable \"mgmtserver_security_group_id\" { default = $mgmtserver_security_group_id }
 variable \"nat_ip\" { default = ${nat_ip} }
 variable \"route53_hosted_zone_id\" { default = ${route53_hosted_zone_id} }
+variable \"app_security_group_id\" { default = ${app_security_group_id} }
 "
