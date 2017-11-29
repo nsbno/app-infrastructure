1fff0b70f4cd29888562d51c25fc2b07b31f05ec
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2017-11-29 16:15:38 +0100

    removed restriction typ whitelist

--- modules/cdn/main.tf
+++ modules/cdn/main.tf
@@ -32,15 +32,13 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   }
   retain_on_delete = "false"
   viewer_certificate {
-     iam_certificate_id       = true
      acm_certificate_arn      = "${data.aws_acm_certificate.certificate.arn}"
      ssl_support_method       = "sni-only"
      minimum_protocol_version = "TLSv1"
   }
   restrictions {
     geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["US", "CA", "EU"]
+      restriction_type = "none"
     }
   }
 }
