611c8006c61ce01c705129a03a2f240874b493a6
Emil Lunde <emil.lunde@bekk.no>
2018-01-04 13:32:23 +0100

    Added instance profile module to be used by elastic beanstalk apps.

---
+++ modules/instance_profile/main.tf
@@ -0,0 +1,40 @@
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.instance_profile_name}"
+  role = "${aws_iam_role.instance_profile_role.name}"
+}
+
+resource "aws_iam_role" "instance_profile_role" {
+  name = "${var.role_name}"
+  description = "${var.role_description}"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "instance_profile_default_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+  role = "${aws_iam_role.instance_profile_role.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "instance_profile_additional_policy_attachment" {
+  policy_arn = "${aws_iam_policy.instance_profile_additional_policy.arn}"
+  role = "${aws_iam_role.instance_profile_role.name}"
+}
+
+resource "aws_iam_policy" "instance_profile_additional_policy" {
+  name = "${var.additional_policy_name}"
+  description = "${var.additional_policy_description}"
+  policy = "${var.additional_policy}"
+}
+
