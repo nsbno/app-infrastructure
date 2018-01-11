3e401c4e544a08a381b051591d9d289bfb91eb02
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2018-01-11 09:47:26 +0100

    Added content type to aws_s3_bucket_object

--- modules/s3_bucket_object/main.tf
+++ modules/s3_bucket_object/main.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket_object" "object" {
-  bucket = "${var.bucket_name}"
-  key    = "${var.bucket_key}"
-  source = "${var.bucket_file}"
-  etag   = "${md5(file("${var.bucket_file}"))}"
+  bucket       = "${var.bucket_name}"
+  key          = "${var.bucket_key}"
+  source       = "${var.bucket_file}"
+  etag         = "${md5(file("${var.bucket_file}"))}"
+  content_type = "${var.content_type}"
 }
--- modules/s3_bucket_object/vars.tf
+++ modules/s3_bucket_object/vars.tf
@@ -1,4 +1,4 @@
 variable "bucket_name" {}
 variable "bucket_key" {}
 variable "bucket_file" {}
-
+variable "content_type" { default = "text/html" }
