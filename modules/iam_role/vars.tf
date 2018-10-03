239e75de9995d8c810d4a97ae27770c5b2d05ea0
Eirik Wiig <eirik.wiig@bekk.no>
2018-10-03 15:45:08 +0200

    Add variable for iam role to set max duration

--- modules/iam_role/vars.tf
+++ modules/iam_role/vars.tf
@@ -1,3 +1,10 @@
 variable "role_name" {}
 variable "role_policy" {}
-variable "description" { default = "" }
+
+variable "description" {
+  default = ""
+}
+
+variable "max_session_duration" {
+  default = "3600"
+}
