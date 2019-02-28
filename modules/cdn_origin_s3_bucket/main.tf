fe7f9a744b7a2c43588b3fbdce557dedf86d79b8
Jarle Holtan <jarle.holtan@bekk.no>
2019-02-28 11:48:09 +0100

    Add Sid to policy statements

--- modules/cdn_origin_s3_bucket/main.tf
+++ modules/cdn_origin_s3_bucket/main.tf
@@ -13,6 +13,7 @@ data "aws_nat_gateway" "test_gateway" {
 
 data "aws_iam_policy_document" "cdn_access_s3_content_policy" {
   statement {
+    sid       = "1"
     actions   = ["s3:GetObject", "s3:ListBucket"]
     resources = ["arn:aws:s3:::${var.bucket_name}/*", "arn:aws:s3:::${var.bucket_name}"]
 
@@ -22,6 +23,7 @@ data "aws_iam_policy_document" "cdn_access_s3_content_policy" {
     }
   }
   statement {
+    sid       = "2"
     actions   = ["s3:GetObject", "s3:ListBucket"]
     resources = ["arn:aws:s3:::${var.bucket_name}/*", "arn:aws:s3:::${var.bucket_name}"]
 
