ad5279eab6bcd250acd3a255e9db8944477955c6
Jarle Holtan <jarle.holtan@bekk.no>
2019-04-09 12:20:48 +0200

    Remove policy for read access to test NAT gw

--- modules/cdn_origin_s3_bucket/main.tf
+++ modules/cdn_origin_s3_bucket/main.tf
@@ -12,10 +12,6 @@ resource "aws_s3_bucket" "cdn_origin_s3_bucket" {
   }
 }
 
-data "aws_nat_gateway" "test_gateway" {
-  subnet_id = "subnet-5cc7ce34"
-}
-
 data "aws_iam_policy_document" "cdn_access_s3_content_policy" {
   statement {
     sid       = "1"
@@ -25,26 +21,6 @@ data "aws_iam_policy_document" "cdn_access_s3_content_policy" {
     principals {
       identifiers = ["${var.origin_access_id_arn}"]
       type        = "AWS"
-    }
-  }
-
-  statement {
-    sid       = "2"
-    actions   = ["s3:GetObject", "s3:ListBucket"]
-    resources = ["arn:aws:s3:::${var.bucket_name}/*", "arn:aws:s3:::${var.bucket_name}"]
-
-    principals {
-      identifiers = ["*"]
-      type        = "*"
-    }
-
-    condition {
-      test     = "IpAddress"
-      variable = "aws:SourceIp"
-
-      values = [
-        "${data.aws_nat_gateway.test_gateway.public_ip}/32",
-      ]
     }
   }
 }
