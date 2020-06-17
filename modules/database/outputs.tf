d00b9f1ffa85fe3a735c70e76ad20654c580c875
Emil Lunde <emil.lunde@bekk.no>
2020-06-17 10:39:41 +0200

    Add arn for username and password in parameter store to database module's output

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -5,3 +5,11 @@ output "db_security_group_id" {
 output "address" {
   value = aws_route53_record.db_internal_route53_record.fqdn
 }
+
+output "ssm_username_arn" {
+  value = aws_ssm_parameter.rds_username.arn
+}
+
+output "ssm_password_arn" {
+  value = aws_ssm_parameter.rds_password.arn
+}
