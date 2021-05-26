c66f49c9db38689747160e1af0367e9c8df88e7a
Emil Lunde <emil.lunde@bekk.no>
2021-05-26 15:45:49 +0200

    Expose port number

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -2,6 +2,10 @@ output "address" {
   value = aws_route53_record.db_internal_route53_record.fqdn
 }
 
+output "port" {
+  value = aws_db_instance.db.port
+}
+
 output "ssm_username_arn" {
   value = aws_ssm_parameter.rds_username.arn
 }
