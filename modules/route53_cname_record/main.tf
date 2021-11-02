a2196910e1c26d1e676dacd52af0f7c183a6b23a
Jarle Holtan <jarle.holtan@bekk.no>
2021-11-02 13:23:54 +0100

    Use weighted routing policy on CNAME records in preparation for migration (#44)
    Use weighted routing policy on CNAME records in preparation for migration

--- modules/route53_cname_record/main.tf
+++ modules/route53_cname_record/main.tf
@@ -2,11 +2,20 @@ data "aws_route53_zone" "route53_zone" {
   name = var.route53_zone
 }
 
+
 resource "aws_route53_record" "environment_route53_record" {
-  zone_id = data.aws_route53_zone.route53_zone.zone_id
-  name    = var.route53_record_name
-  type    = "CNAME"
-  ttl     = var.route53_record_ttl
-  records = [var.route53_record]
+  zone_id        = data.aws_route53_zone.route53_zone.zone_id
+  name           = var.route53_record_name
+  type           = "CNAME"
+  ttl            = var.route53_record_ttl
+  records        = [var.route53_record]
+  set_identifier = var.set_identifier
+
+  dynamic "weighted_routing_policy" {
+    for_each = var.use_weighted_routing ? [1] : []
+    content {
+      weight = var.weight
+    }
+  }
 }
 
--- modules/route53_cname_record/vars.tf
+++ modules/route53_cname_record/vars.tf
@@ -13,3 +13,17 @@ variable "route53_record" {
 variable "route53_record_ttl" {
 }
 
+variable "set_identifier" {
+  type    = string
+  default = null
+}
+
+variable "use_weighted_routing" {
+  type    = bool
+  default = false
+}
+
+variable "weight" {
+  type    = number
+  default = 100
+}
